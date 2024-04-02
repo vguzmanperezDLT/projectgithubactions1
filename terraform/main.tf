e30771ab6d6593eee6d6c34597133c394f0b035f
Victor Daniel <vguzmanperez@deloitte.com>
2024-04-02 16:09:39 -0600

    update names

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t2.micro"
 
   tags = {
